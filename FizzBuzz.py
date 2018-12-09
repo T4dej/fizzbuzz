b7a4139b76a103dbef1efdad0a7f70e39e3991f4
Tadej Ciglič <tadej.ciglic@gmail.com>
2018-12-09 23:54:58 +0100

    update zanke s preverjanjem

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,19 +2,25 @@
 
 num1 = raw_input("Vnesite celo število med 1 in 100: ")
 
+def zanka():
+    for x in range(1, num1 + 1):
+        if x % 3 == 0 and x % 5 == 0:
+            print("FizzBuzz")
+        elif x % 3 == 0:
+            print("fizz")
+        elif x % 5 == 0:
+            print("buzz")
+        else:
+            print(x)
+
 try:
     num1 = int(num1)
     if num1 > 100:
-        print("Število večje od 100!")
+        print("Izbrali ste število večje od 100! Izbrano število je spremenjeno v 100.")
+        num1 = 100
+        zanka()
     else:
-        for x in range(1, num1+1):
-            if x % 3 == 0 and x % 5 == 0:
-                print("FizzBuzz")
-            elif x % 3 == 0:
-                print("fizz")
-            elif x % 5 == 0:
-                print("buzz")
-            else:
-                print(x)
+        zanka()
+
 except ValueError:
     print("Vnesite celo število!")
